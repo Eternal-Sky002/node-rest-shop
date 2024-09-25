b14d7838999d72bef90f258dd78c72d2c4562979
ETERNALSKY-002 <sebastianthekuroshitsuji@gmail.com>
2024-09-25 16:15:25 +0700

    connection to mongodb

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRouters = require('./api/routes/products');
 const orderRouters = require('./api/routes/orders');
+
+// Konfigurasi MongoDB
+mongoose.connect(
+    'mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop.lusvh.mongodb.net/?retryWrites=true&w=majority&appName=node-rest-shop'
+)
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
